d593252bd122a27b9613f74cb8f9bccc2633ac6c
Vlad Bokov <razum2um@mail.ru>
2017-09-04 05:49:30 +0700

    init

---
+++ lib/rails/session_cookie.rb
@@ -0,0 +1,7 @@
+require "rails/session_cookie/version"
+
+module Rails
+  module SessionCookie
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/session_cookie/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module SessionCookie
+    VERSION = "0.1.0"
+  end
+end
